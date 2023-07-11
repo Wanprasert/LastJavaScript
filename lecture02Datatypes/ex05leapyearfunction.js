53211cb070c02b93c7024b5a09697d3a9e2749b3
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-11 19:08:03 +0700

    update

--- lecture02Datatypes/ex05leapyearfunction.js
+++ lecture02Datatypes/ex05leapyearfunction.js
@@ -10,3 +10,6 @@ if (x % 4 ==0) {
 console.log(leapYear(1990)) //false
 console.log(leapYear(1924)) //true
 console.log(leapYear(2021)) //false
+
+console.log(leapYear(2020)) //false
+console.log(leapYear(2000)) //false
