e3daca5573c1a62ab4c9999fb5fffa4446b9f14f
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-11 19:18:11 +0700

    update

---
+++ lecture02Datatypes/ex06.js
@@ -0,0 +1,9 @@
+    // Function to Find the Parameter of Triangle
+    function perimeter(a, b) {
+        let perimeter = 2 * (a + b);
+        return console.log("findPerimeter = " + perimeter);
+    }
+
+perimeter(6, 7);
+perimeter(20, 10);
+perimeter(2, 9);
---
+++ lecture02Datatypes/ex07.js
@@ -0,0 +1,8 @@
+function equation(num) {
+    total =eval(num);
+    return console.log(total);
+}
+
+equation("1+1");
+equation("7*4-2");
+equation("1+1+1+1+1");
