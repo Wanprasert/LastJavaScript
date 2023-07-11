4a547ba4c46aab303d81d23c10304d48fd772f38
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-11 17:56:46 +0700

    update

---
+++ lecture02Datatypes/ex02.js
@@ -0,0 +1,7 @@
+function animals(chic, cows, pig) {
+    return (chic * 2) + (cows * 4) + (pig * 4);
+  }
+
+  console.log(animals(2, 3, 5)); //36
+  console.log(animals(1, 2, 3)); //22
+  console.log(animals(5, 2, 8)); //50
