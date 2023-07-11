557758b7960fa5a9b4c56f6779dc5b1f33e09bef
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-11 17:56:55 +0700

    update

---
+++ lecture02Datatypes/ex01.js
@@ -0,0 +1,22 @@
+function lessThan100(x, y) {
+if (x + y <= 100) {
+    return true;
+}
+
+else  {
+    return false;
+}
+
+}
+
+console.log(lessThan100(22,15)) ;//true
+console.log(lessThan100(83,34)) ;//false
+console.log(lessThan100(3, 77)) ;//true
+
+/*function lessThan100(num1, num2) {
+    return num1 + num2 < 100;
+  }
+  
+  console.log(lessThan100(22, 15));  // Output: true
+  console.log(lessThan100(83, 34));  // Output: false
+  console.log(lessThan100(3, 77));   // Output: true*/
