d7cbb7c50414aeec583964f9e356c756b7a52236
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-11 16:56:46 +0700

    update

---
+++ lecture02Datatypes/6strtemplitr.js
@@ -0,0 +1,15 @@
+//Concatenation with template literals
+console.log("\n********Concatenation with tempate literals********\n")
+
+let str1 = "JavaScript"
+let str2 = "fun";
+
+console.log(`I am writting code in ${str1}`);
+console.log(`Formatting in ${str1} is ${str2}!`);
+
+//Expression in template literals
+console.log("\n********Expression in template literals********\n")
+
+let bool1 = true;
+console.log(`1 + 1 is ${1 + 1}`);
+console.log(`The opposite of true is ${!bool1}`);
