2366f38e93120d4d3b50ca8e34f1e7453414073d
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-11 18:02:33 +0700

    update

---
+++ lecture02Datatypes/18function.js
@@ -0,0 +1,11 @@
+// 1. Function Definition
+function printHello(name){
+    console.log("Hello "+name);
+    return name +" hello!";
+}
+
+console.log(printHello.name)
+
+// 2. Function Invocation
+let result = printHello("Wanprasert !");
+console.log(result);
