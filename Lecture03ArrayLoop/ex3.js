b78a719ffc1ac2990abb80282b2e9568522004da
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-18 19:16:25 +0700

    update

---
+++ Lecture03ArrayLoop/ex3.js
@@ -0,0 +1,12 @@
+function indexMultiplier(array) {
+  let result = 0;
+
+  for (let index = 0; index < array.length; index++) {
+    result += array[index] * index;
+  }
+
+  return result;
+}
+
+console.log(indexMultiplier([1, 2, 3, 4, 5])); // 40
+console.log(indexMultiplier([-3, 0, 8, -6])); // -2
