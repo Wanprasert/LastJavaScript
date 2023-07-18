fa37befb27715c8461b4af4b54486d5e90c7a54b
Wanprasert <s6206021621171@email.kmutnb.ac.th>
2023-07-18 18:53:24 +0700

    update

--- Lecture03ArrayLoop/ex1.js
+++ Lecture03ArrayLoop/ex1.js
@@ -1,12 +1,35 @@
+let values1 = ['Apple', 1, false];
+let values2 = ['Fries', 2, true];
+let values3 = ['Mars', 9, 'Apple'];
 
-let commonElements = [];
+let i=[];
 
-for (let i = 0; i < values1.length; i++) {
-  let element = values1[i];
-
-  if (values2.includes(element) && values3.includes(element)) {
-    commonElements.push(element);
-  }
+for (let index = 0; index < values1.length; index++) {
+    const x = values1[index];
+    for (let index = 0; index < values2.length; index++) {
+        const y = values2[index];
+        if (x===y) {
+            console.log("values1 and values2 : ",out=x);
+        }
+    }
 }
 
-console.log(commonElements);
+for (let index = 0; index < values1.length; index++) {
+    const x = values1[index];
+    for (let index = 0; index < values3.length; index++) {
+        const y = values3[index];
+        if (x===y) {
+            console.log("values1 and values3 : ",out=x);
+        }
+    }
+}
+
+for (let index = 0; index < values2.length; index++) {
+    const x = values2[index];
+    for (let index = 0; index < values3.length; index++) {
+        const y = values3[index];
+        if (x===y) {
+            console.log("values2 and values3 : ",out=x);
+        }
+    }
+}
