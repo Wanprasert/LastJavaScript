4812595db6dfff8613d44c5cc942cb259d60c1d8
Your Name <you@example.com>
2023-07-24 21:28:29 +0700

    update

--- Lecture03ArrayLoop/ex5.js
+++ Lecture03ArrayLoop/ex5.js
@@ -4,3 +4,4 @@ countPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
 countPosSumNeg([92, 6, 73, -77, 81, -90, 99, 8, -85, 34]) =[7, -252]
 countPosSumNeg([91, -4, 80, -73, -28]) = [2, 105]
 countPosSumNeg([]) = []
+asdasd
